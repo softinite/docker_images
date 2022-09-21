8c1745fc0ede134e163ead0ade7757eada169b74
Sergiu Ivasenco <contact@softinite.com>
2022-09-21 13:42:56 -0400

    Upgraded base image

--- dokuwiki/runDokuWiki.sh
+++ dokuwiki/runDokuWiki.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #NOTE: one may need to replace relative path with absolute
-docker run -d -p 9001:80 -v "~/Documents/dokuwiki/data:/dokuwiki/data/" --name dokuwiki_c softinite/dokuwiki:2.0.3
+docker run -d -p 9001:80 -v "~/Documents/dokuwiki/data:/dokuwiki/data/" --name dokuwiki_c softinite/dokuwiki:2.0.4
 #docker exec dokuwiki_c mkdir /dokuwiki/data/pages
 #docker exec dokuwiki_c mkdir /dokuwiki/data/media_attic
 #docker exec dokuwiki_c mkdir /dokuwiki/data/attic
